04a7bcc2a883cd56445cb13eb9a411d8237880af
Vaghinak Papoyan <50072682+Anonimus1423@users.noreply.github.com>
2022-05-06 11:12:28 -0700

    asdfasdf

--- src/components/fisrt/index.jsx
+++ src/components/fisrt/index.jsx
@@ -1,20 +1,22 @@
 import React,{ useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { clearFirstVisit } from '../../func/isFirstVisit'
 import './index.scss'
 
 export function First(){
     const [page,setPage] = useState(0)
-    function changePage(){
-        let thisPage = page
-        setPage(3)
+    const naviagte = useNavigate()
+    function changePage(pageNext){
+        setPage('loader')
         setTimeout(()=>{
-            setPage(thisPage+1)
-        },2000)
+            setPage(pageNext)
+        },500)
     }
     if(page === 0){
         return(
             <div className='background__schoole'>
-                <div className="background__schoole__title">HELLO...</div>
-                <button className='btn btn-next' onClick={()=>changePage()}>Next</button>
+                <div className="background__schoole__title"><span>H</span><span>E</span><span>L</span><span>L</span><span>O</span><span>!</span></div>
+                <button className='btn btn-next' onClick={()=>changePage(page+1)}>Next</button>
             </div>
         )
     }
@@ -22,14 +24,33 @@ export function First(){
         return(
             <div className='background__schoole'>
                 <div className="background__schoole__title">For first create teams</div>
-                <button className='btn btn-next' onClick={()=>changePage()}>Next</button>
-                <button className='btn btn-back' onClick={()=>setPage(page-1)}>Back</button>
+                <button className='btn btn-next' onClick={()=>changePage(page+1)}>Next</button>
+                <button className='btn btn-back' onClick={()=>changePage(page-1)}>Back</button>
+            </div>
+        )
+    }
+    if(page === 2){
+        return(
+            <div className='background__schoole'>
+                <div className="background__schoole__title">Second select more settings</div>
+                <button className='btn btn-next' onClick={()=>changePage(page+1)}>Next</button>
+                <button className='btn btn-back' onClick={()=>changePage(page-1)}>Back</button>
+            </div>
+        )
+    }
+    if(page === 3){
+        return(
+            <div className='background__schoole'>
+                <div className="background__schoole__title">Click to start button</div>
+                <button  className='btn btn-next' onClick={()=>clearFirstVisit(naviagte)}>{`Let's Go`}</button>
             </div>
         )
     }
         return(
             <div className='background__schoole'>
-                <div className="background__schoole__title">Loading...</div>
+                <div className="background__schoole__title">
+                <div id="loading-bar-spinner" className="spinner"><div className="spinner-icon"></div></div>
+                </div>
             </div>
         )
 }
